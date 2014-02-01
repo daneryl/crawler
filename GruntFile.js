14b87ce912e4f5b3c3fc711c6c1bea371c5fc5bc
daneryl <daneryl@gmail.com>
2014-02-01 16:41:20 -0500

    grunt tasks for test and jshint setup

---
+++ GruntFile.js
@@ -0,0 +1,56 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-jasmine-node-coverage');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Project configuration.
+  grunt.initConfig({
+    jasmine_node: {
+      coverage: {
+        excludes: []
+      },
+      options: {
+        forceExit: true,
+        match: '.',
+        specFolders: ['specs'],
+        extensions: 'js',
+        specNameMatcher: 'spec',
+        isVerbose: false,
+        //junitreport: {
+          //report: true,
+          //savePath : './junit_reports/',
+          //useDotNotation: true,
+          //consolidate: true
+        //}
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['**/**.js', '!coverage/lcov-report/prettify.js'],
+        tasks: ['default'],
+        options: {
+          spawn: true,
+        },
+      },
+    },
+    jshint: {
+      files: ['GruntFile.js', 'specs/**.js', 'src/**.js'],
+      options: {
+        jshintrc: '.jshintrc',
+      }
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('test', 'jasmine_node');
+
+
+  grunt.registerTask('timestamp', function() {
+    grunt.log.subhead(Date());
+  });
+
+};
